a2e837fb52bed469b16047827ee0e8ab5499d395
RJEcomerce <errejota.ecomerce@gmail.com>
2025-05-26 22:34:27 +0000

    up

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './contexts/AuthContext';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Navbar from './components/Navbar';
@@ -29,7 +30,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <Router>
           <div className="min-h-screen flex flex-col bg-dark-800">
             <Navbar />
             <div className="flex-grow">
@@ -55,7 +56,7 @@ const App = () => (
               </Routes>
             </div>
           </div>
-        </BrowserRouter>
+        </Router>
       </TooltipProvider>
     </AuthProvider>
   </QueryClientProvider>
