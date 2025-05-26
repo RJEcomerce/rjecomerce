6d317d20b05267c9501fe1d200f657c1a05b1c04
RJEcomerce <errejota.ecomerce@gmail.com>
2025-05-26 22:32:30 +0000

    Updates

---
+++ assets/browser-DcfoqgtK.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-F7F2bKqX.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
